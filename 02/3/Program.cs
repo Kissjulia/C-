eddbac1a19035718e0eb88023a70f779ea8ea3d4
Юлия <>
2022-09-02 15:05:38 +0300

    3 task

---
+++ 02/3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите чило A:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+
+    Console.WriteLine("END");
