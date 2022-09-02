83d53b74164f85cd990ace847cae13ac877e3141
Юлия <>
2022-09-02 14:30:53 +0300

    2я задача

---
+++ 02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите чило A:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите чило B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите чило C:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int maxnum = a;
+
+if (maxnum < b)
+{
+    maxnum = b;
+}
+if (maxnum < c)
+{
+    maxnum = c;
+}
+Console.WriteLine(maxnum);
