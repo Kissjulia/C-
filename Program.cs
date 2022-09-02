bbefba4181d2582fa5015d230af0420cdf37f201
Юлия <>
2022-09-02 13:23:39 +0300

    1я задача

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите чило A:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите чило B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+else
+{
+    Console.WriteLine($"{b} больше {a}");
+}
