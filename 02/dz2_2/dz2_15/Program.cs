b9e4db8d179f659f594a4805d690a90eef9b07fe
Юлия <>
2022-09-06 13:04:04 +0300

    15task

---
+++ 02/dz2_2/dz2_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите цифру дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0 && num < 6)
+{
+Console.WriteLine("нет");
+}
+
+else {
+    
+Console.WriteLine("да");
+}
+
