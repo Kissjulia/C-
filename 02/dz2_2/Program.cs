171f869cd981e702403aa6efc420a9c1a9d8e7d1
Юлия <>
2022-09-06 12:33:29 +0300

    2d task

---
+++ 02/dz2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите чило");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (99 < num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    num %= 10;
+    Console.WriteLine(num);
+}
+else
+    Console.WriteLine("Третьей цифры нет");
