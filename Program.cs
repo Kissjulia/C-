370d1cfd78d29d4ae3a96dec19647055aaa09a29
Kissjulia <112159185+Kissjulia@users.noreply.github.com>
2022-09-11 21:20:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿Console.WriteLine("Введите координаты точки А");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
-int z1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int kub = 1;
 
-Console.WriteLine("Введите координаты точки B");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-int z2 = Convert.ToInt32(Console.ReadLine());
-
-double AB = Math.Sqrt (Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine(AB);
+while (i<=N)
+{
+    kub = i * i * i;
+    i++;
+    Console.WriteLine($"Куб {kub}");
+}
